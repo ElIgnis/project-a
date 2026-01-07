fbf53338d531adc2008bf1af882a4683f825586f
ElIgnis <Intervention.408@live.com>
2026-01-08 00:59:20 +0800

    Set up basic forms and routing

---
+++ src/lib/placeholder-data.ts
@@ -0,0 +1,25 @@
+﻿const customers = [
+  { id: 0 },
+  { id: 1 },
+  { id: 2 },
+  { id: 3 },
+  { id: 4 },
+  { id: 5 },
+  { id: 6 },
+];
+
+const invoices = [
+  {
+    customer_id: customers[0].id,
+    amount: 15795,
+    status: "pending",
+    date: "2022-12-06",
+  },
+  {
+    customer_id: customers[1].id,
+    amount: 20348,
+    status: "pending",
+    date: "2022-11-14",
+  },
+  // ...
+];
