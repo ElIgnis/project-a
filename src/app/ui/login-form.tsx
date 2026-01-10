b43979b768914a35d6ec19fbc6c5c22867e2b920
ElIgnis <Intervention.408@live.com>
2026-01-11 02:36:24 +0800

    Clean up - Removed unused error-code-list.ts - Added back button for login and sign up and adjusted css

--- src/app/ui/login-form.tsx
+++ src/app/ui/login-form.tsx
@@ -5,11 +5,12 @@ import {
   KeyIcon,
   ExclamationCircleIcon,
 } from '@heroicons/react/24/outline';
-import { ArrowRightIcon } from '@heroicons/react/20/solid';
+import { ArrowRightIcon, ArrowLeftIcon } from '@heroicons/react/20/solid';
 import { Button } from '@/app/ui/button';
 import { login } from '@/app/lib/client-actions'
 import { useState } from 'react';
 import { LoginValidationErrors } from '@/../lib/utils/form-validation'
+import Link from 'next/link'
 
 export default function LoginForm() {
 
@@ -42,83 +43,93 @@ export default function LoginForm() {
   return (
     <form onSubmit={handleLogin} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <h1 className= "mb-3 text-2xl text-gray-900">
-          Log into your account
-        </h1>
-        <div className="w-1/6">
-        
-          <div>
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 text-gray-900 placeholder:text-gray-500"
-                id="email"
-                type="email"
-                name="email"
-                placeholder="Enter your email address"
-                required
-                aria-describedby='email-error'
-              />
-              <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-            </div>
-            <div id="email-error" aria-live="polite" aria-atomic="true">
-                {validationErrors?.email &&
-                  validationErrors.email.map((error: string) => (
-                    <p className="mt-2 text-sm text-red-500" key={error}>
-                      {error}
-                    </p>
-                ))}
-            </div>
-          </div>
+        <Link
+            href="/"
+            className="flex w-min justify-center gap-2 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+        >
+            <ArrowLeftIcon className="w-5 md:w-6" />
+            <span>Back</span> 
+        </Link>
+
+        <div className="justify-items-center">
+          <h1 className= "mb-3 text-2xl text-gray-900">
+            Log into your account
+          </h1>
           
-          <div className="mt-4">
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 text-gray-900 placeholder:text-gray-500"
-                id="password"
-                type="password"
-                name="password"
-                placeholder="Enter password"
-                value={passwordInput}
-                onChange={(e)=>{setPasswordInput(e.target.value);}}
-                required
-                minLength={6}
-                aria-describedby='password-error'
-              />
-              <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+          <div className="w-3/6 min-w-96 max-w-2/12">      
+            <div>
+              <label
+                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <div className="relative">
+                <input
+                  className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 text-gray-900 placeholder:text-gray-500"
+                  id="email"
+                  type="email"
+                  name="email"
+                  placeholder="Enter your email address"
+                  required
+                  aria-describedby='email-error'
+                />
+                <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              </div>
+              <div id="email-error" aria-live="polite" aria-atomic="true">
+                  {validationErrors?.email &&
+                    validationErrors.email.map((error: string) => (
+                      <p className="mt-2 text-sm text-red-500" key={error}>
+                        {error}
+                      </p>
+                  ))}
+              </div>
             </div>
-            <div id="password-error" aria-live="polite" aria-atomic="true">
-                {validationErrors?.password &&
-                  validationErrors.password.map((error: string) => (
-                    <p className="mt-2 text-sm text-red-500" key={error}>
-                      {error}
-                    </p>
-                ))}
+            
+            <div className="mt-4">
+              <label
+                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <div className="relative">
+                <input
+                  className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 text-gray-900 placeholder:text-gray-500"
+                  id="password"
+                  type="password"
+                  name="password"
+                  placeholder="Enter password"
+                  value={passwordInput}
+                  onChange={(e)=>{setPasswordInput(e.target.value);}}
+                  required
+                  minLength={6}
+                  aria-describedby='password-error'
+                />
+                <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              </div>
+              <div id="password-error" aria-live="polite" aria-atomic="true">
+                  {validationErrors?.password &&
+                    validationErrors.password.map((error: string) => (
+                      <p className="mt-2 text-sm text-red-500" key={error}>
+                        {error}
+                      </p>
+                  ))}
+              </div>
             </div>
           </div>
-        </div>
-        <input type="hidden" name="redirectTo" />
-        <Button className="mt-4 w-1/6"> 
-          Login <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
-        </Button>
-        
-        <div id="form-submit-error" aria-live="polite" aria-atomic="true">
-          {(validationErrors || loginFailedError) &&
-            <p className="mt-2 text-sm text-red-500">
-                {loginFailedError}
-              </p>
-          }
+          <input type="hidden" name="redirectTo" />
+          <Button className="mt-4 w-min gap-4"> 
+            Login <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+          </Button>
+          
+          <div id="form-submit-error" aria-live="polite" aria-atomic="true">
+            {(validationErrors || loginFailedError) &&
+              <p className="mt-2 text-sm text-red-500">
+                  {loginFailedError}
+                </p>
+            }
+          </div>
         </div>
       </div>
     </form>
--- src/app/ui/signup-form.tsx
+++ src/app/ui/signup-form.tsx
@@ -5,12 +5,13 @@ import {
   KeyIcon,
   ExclamationCircleIcon,
 } from '@heroicons/react/24/outline';
-import { ArrowRightIcon } from '@heroicons/react/20/solid';
+import { ArrowRightIcon, ArrowLeftIcon } from '@heroicons/react/20/solid';
 import { Button } from '@/app/ui/button';
 import { signup } from '@/app/lib/client-actions'
 import { SignUpValidationErrors } from '@/../lib/utils/form-validation'
 import { useState } from 'react';
 import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 
 export default function SignupForm() {
 
@@ -54,144 +55,153 @@ export default function SignupForm() {
   return (
     <form onSubmit={handleSignUp} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <h1 className= "mb-3 text-2xl text-gray-900">
-          Account Sign Up
-        </h1>
-        <div className="w-1/6">
-        <div>
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="name"
-            >
-              Username
-            </label>
-            <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 text-gray-900 placeholder:text-gray-500"
-                id="username"
-                type="text"
-                name="username"
-                placeholder="Enter your username"
-                required
-                aria-describedby='name-error'
-              />
-              <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+        <Link
+            href="/"
+            className="flex w-min justify-center gap-2 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+        >
+            <ArrowLeftIcon className="w-5 md:w-6" />
+            <span>Back</span> 
+        </Link>
+        <div className="justify-items-center">
+          <h1 className= "mb-3 text-2xl text-gray-900">
+            Account Sign Up
+          </h1>
+          <div className="w-3/6 min-w-96 max-w-2/12">
+          <div>
+              <label
+                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                htmlFor="name"
+              >
+                Username
+              </label>
+              <div className="relative">
+                <input
+                  className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 text-gray-900 placeholder:text-gray-500"
+                  id="username"
+                  type="text"
+                  name="username"
+                  placeholder="Enter your username"
+                  required
+                  aria-describedby='name-error'
+                />
+                <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              </div>
+              <div id="name-error" aria-live="polite" aria-atomic="true">
+                  {validationErrors?.name &&
+                    validationErrors.name.map((error: string) => (
+                      <p className="mt-2 text-sm text-red-500" key={error}>
+                        {error}
+                      </p>
+                  ))}
+              </div>
             </div>
-            <div id="name-error" aria-live="polite" aria-atomic="true">
-                {validationErrors?.name &&
-                  validationErrors.name.map((error: string) => (
+
+            <div>
+              <label
+                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <div className="relative">
+                <input
+                  className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 text-gray-900 placeholder:text-gray-500"
+                  id="email"
+                  type="email"
+                  name="email"
+                  placeholder="Enter your email address"
+                  required
+                  aria-describedby='email-error'
+                />
+                <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              </div>
+              <div id="email-error" aria-live="polite" aria-atomic="true">
+                {validationErrors?.email &&
+                  validationErrors.email.map((error: string) => (
                     <p className="mt-2 text-sm text-red-500" key={error}>
                       {error}
                     </p>
                 ))}
             </div>
-          </div>
-
-          <div>
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 text-gray-900 placeholder:text-gray-500"
-                id="email"
-                type="email"
-                name="email"
-                placeholder="Enter your email address"
-                required
-                aria-describedby='email-error'
-              />
-              <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
-            <div id="email-error" aria-live="polite" aria-atomic="true">
-              {validationErrors?.email &&
-                validationErrors.email.map((error: string) => (
-                  <p className="mt-2 text-sm text-red-500" key={error}>
-                    {error}
-                  </p>
-              ))}
+            
+            <div className="mt-4">
+              <label
+                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <div className="relative">
+                <input
+                  className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 text-gray-900 placeholder:text-gray-500"
+                  id="password"
+                  type="password"
+                  name="password"
+                  placeholder="Enter password"
+                  required
+                  minLength={8}
+                  value={passwordInput}
+                  onChange={e=>{setPasswordInput(e.target.value)}}
+                  aria-describedby='password-error'
+                />
+                <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              </div>
+              <div id="password-error" aria-live="polite" aria-atomic="true">
+                  {validationErrors?.password &&
+                    validationErrors.password.map((error: string) => (
+                      <p className="mt-2 text-sm text-red-500" key={error}>
+                        {error}
+                      </p>
+                  ))}
+              </div>
+            </div>
+
+            <div>
+              <label
+                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                htmlFor="confirmPassword"
+              >
+                Confirm Password
+              </label>
+              <div className="relative">
+                <input
+                  className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 text-gray-900 placeholder:text-gray-500"
+                  id="confirmPassword"
+                  type="password"
+                  name="confirmPassword"
+                  placeholder="Re-enter your password"
+                  value={confirmPasswordInput}
+                  onChange={e=>{setConfirmPasswordInput(e.target.value)}}
+                  minLength={8}
+                  required
+                  aria-describedby='confirm-password-error'
+                />
+                <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              </div>
+            </div>
+            <div id="confirm-password-error" aria-live="polite" aria-atomic="true">
+                {validationErrors?.confirmPassword &&
+                  validationErrors.confirmPassword.map((error: string) => (
+                    <p className="mt-2 text-sm text-red-500" key={error}>
+                      {error}
+                    </p>
+                ))}
+            </div>
+
           </div>
-          </div>
+          <input type="hidden" name="redirectTo" />
+          <Button className="mt-4 w-min gap-4 text-nowrap"> 
+            Sign Up <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+          </Button>
           
-          <div className="mt-4">
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 text-gray-900 placeholder:text-gray-500"
-                id="password"
-                type="password"
-                name="password"
-                placeholder="Enter password"
-                required
-                minLength={8}
-                value={passwordInput}
-                onChange={e=>{setPasswordInput(e.target.value)}}
-                aria-describedby='password-error'
-              />
-              <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-            </div>
-            <div id="password-error" aria-live="polite" aria-atomic="true">
-                {validationErrors?.password &&
-                  validationErrors.password.map((error: string) => (
-                    <p className="mt-2 text-sm text-red-500" key={error}>
-                      {error}
-                    </p>
-                ))}
-            </div>
+          <div id="form-submit-error" aria-live="polite" aria-atomic="true">
+            {(validationErrors || signupFailedError) &&
+              <p className="mt-2 text-sm text-red-500">
+                  {signupFailedError}
+              </p>
+            }
           </div>
-
-          <div>
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="confirmPassword"
-            >
-              Confirm Password
-            </label>
-            <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 text-gray-900 placeholder:text-gray-500"
-                id="confirmPassword"
-                type="password"
-                name="confirmPassword"
-                placeholder="Re-enter your password"
-                value={confirmPasswordInput}
-                onChange={e=>{setConfirmPasswordInput(e.target.value)}}
-                minLength={8}
-                required
-                aria-describedby='confirm-password-error'
-              />
-              <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-            </div>
-          </div>
-          <div id="confirm-password-error" aria-live="polite" aria-atomic="true">
-              {validationErrors?.confirmPassword &&
-                validationErrors.confirmPassword.map((error: string) => (
-                  <p className="mt-2 text-sm text-red-500" key={error}>
-                    {error}
-                  </p>
-              ))}
-          </div>
-
-        </div>
-        <input type="hidden" name="redirectTo" />
-        <Button className="mt-4 w-1/6"> 
-          Sign Up <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
-        </Button>
-        
-        <div id="form-submit-error" aria-live="polite" aria-atomic="true">
-          {(validationErrors || signupFailedError) &&
-            <p className="mt-2 text-sm text-red-500">
-                {signupFailedError}
-            </p>
-          }
         </div>
       </div>
     </form>
