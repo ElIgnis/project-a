3b98b02c09d7c8aef46bf877eb21fce3be452c35
ElIgnis <Intervention.408@live.com>
2026-01-18 01:29:28 +0800

    Fixed wrong file for auth

--- src/app/(auth-required)/dashboard/page.tsx
+++ src/app/(auth-required)/dashboard/page.tsx
@@ -1,20 +1,6 @@
-﻿
-import { headers } from 'next/headers'
-import { auth } from '@/../auth'
-import { redirect } from 'next/navigation';
-import { PostingBoard } from '@/app/ui/posting-board/posting-board'
+﻿import { PostingBoard } from '@/app/ui/posting-board/posting-board'
 
 export default async function Page() {
-  const session = await auth.api.getSession({
-    headers: await headers()
-  })
-
-  console.log(session);
-
-  if(!session) {
-    console.log("a");
-    redirect('/login');
-  }
   return (
     <>
       <PostingBoard/>
